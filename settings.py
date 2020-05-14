514366aa91114616b3e7cb3554713e58ee185696
Lucas Rodriguez <rodriguez.lucas.e@gmail.com>
2020-05-14 16:56:01 -0300

    Refactor configs and add production

--- settings.py
+++ settings.py
@@ -9,49 +9,60 @@ class Config:
     DEBUG = False
     TESTING = False
     ENV = os.getenv("ENVIRONMENT")
-
-    @classmethod
-    def for_actual_environment(cls):
-        all_config_objects = [TestConfig, DevelopmentConfig]
-        for config_object in all_config_objects:
-            if config_object.is_correct_for(cls.ENV):
-                return config_object
-
-        return DevelopmentConfig
-
-    @classmethod
-    def is_correct_for(cls, environment_name):
-        raise NotImplementedError
-
-    @classmethod
-    def get_cache_client(cls, cache_factory):
-        raise NotImplementedError
-
-
-class TestConfig(Config):
-    TESTING = True
-    ENV = os.getenv("ENVIRONMENT")
-
-    @classmethod
-    def is_correct_for(cls, environment_name):
-        return environment_name == 'testing'
-
-    @classmethod
-    def get_cache_client(cls, cache_factory):
-        return cache_factory.null_client()
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    ENV = os.getenv("ENVIRONMENT")
     HERE_API_KEY = os.getenv('HERE_API_KEY')
     CACHE_SERVER = (os.getenv("CACHE_SERVER_HOST"), int(os.getenv("CACHE_SERVER_PORT")))
     BSAS_TRANSPORT_API_CLIENT_ID = os.getenv('BSAS_TRANSPORT_API_CLIENT_ID')
     BSAS_TRANSPORT_API_SECRET = os.getenv('BSAS_TRANSPORT_API_SECRET')
 
     @classmethod
+    def for_actual_environment(cls):
+        all_config_objects = [DevelopmentConfig, ProductionConfig]
+        for config_object in all_config_objects:
+            if config_object.is_correct_for(cls.ENV):
+                return config_object
+
+        all_environments_names = ', '.join(config.environment_name() for config in all_config_objects)
+        raise Exception(f"No config found for environment: {cls.ENV}. Available environments: {all_environments_names}")
+
+    @classmethod
     def is_correct_for(cls, environment_name):
-        return environment_name == 'development'
+        raise NotImplementedError
+
+    @classmethod
+    def environment_name(cls):
+        raise NotImplementedError
+
+    @classmethod
+    def get_cache_client(cls, cache_factory):
+        raise NotImplementedError
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    @classmethod
+    def environment_name(cls):
+        return 'development'
+
+    @classmethod
+    def is_correct_for(cls, environment_name):
+        return environment_name == cls.environment_name()
+
+    @classmethod
+    def get_cache_client(cls, cache_factory):
+        return cache_factory.memcached_client(server=cls.CACHE_SERVER)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+    @classmethod
+    def environment_name(cls):
+        return 'production'
+
+    @classmethod
+    def is_correct_for(cls, environment_name):
+        return environment_name == cls.environment_name()
 
     @classmethod
     def get_cache_client(cls, cache_factory):
